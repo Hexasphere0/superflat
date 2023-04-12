89b9560fc26df55026e3bd567d2e0ed4b7216173
Hexasphere0 <kesavgurugubelli@gmail.com>
2023-04-12 13:19:34 -0700

    add custom ae2 processors

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -3,8 +3,35 @@
 console.info('Hello, World! (You will only see this line once in console, during startup)')
 
 onEvent('item.registry', event => {
-	event.create("rainbow_petal").displayName("Rainbow Petal");
-	event.create("undead_transmutation_catalyst").displayName("Undead Transmutation Catalyst").maxDamage(100);
+	event.create("rainbow_petal").displayName("Rainbow Petal")
+	event.create("undead_transmutation_catalyst").displayName("Undead Transmutation Catalyst").maxDamage(100)
+
+	event.create("printed_compatability_circuit")
+	event.create("compatability_processor").displayName("Compatability Processor")
+	
+	event.create("printed_estimation_circuit")
+	event.create("estimation_processor").displayName("Estimation Processor")
+
+	event.create("printed_duplication_circuit")
+	event.create("duplication_processor").displayName("Duplication Processor")
+
+	event.create("printed_differential_circuit")
+	event.create("differential_processor").displayName("Differential Processor")
+
+	event.create("printed_matrix_circit")
+	event.create("matrix_processor").displayName("Matrix Processor")
+
+	event.create("printed_recursive_circuit")
+	event.create("recursive_processor").displayName("Recursive Processor")
+
+	event.create("printed_clearance_circuit")
+	event.create("clearance_processor").displayName("Clearance Processor")
+
+	event.create("printed_probability_circuit")
+	event.create("probability_processor").displayName("Probability Processor")
+
+	event.create("printed_time_circuit")
+	event.create("time_processor").displayName("Time Processor")
 })
 
 onEvent('block.registry', event => {
